120b9d7c5a7b7930b411c647f7c184c46d41f968
JiangJiunnYih <129246883+JiangJiunnYih@users.noreply.github.com>
2023-07-31 11:50:14 +0800

    Update pysendmail.py

--- pysendmail.py
+++ pysendmail.py
@@ -1,17 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas
+import numpy as np
 import smtplib, ssl
-
-
-port = 465  # For SSL
+                    
+port = 465 
 smtp_server = "smtp.gmail.com"
-sender_email = "lab814reservatiosystem@gmail.com"  # Enter your address
-receiver_email = "olivereddie1996.mg10@nycu.edu.tw"  # Enter receiver address
+sender_email = "lab814reservatiosystem@gmail.com"
+receiver_email = "olivereddie1996.mg10@nycu.edu.tw"
 password = "elqskdpnokjjwzoi"
 message = """\
-Subject: Hi there
+            Subject: Hi there
 
-This message is sent from Python."""
+            This message is sent from Python."""
 
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+try:
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        print("Mail send Successed")
+except:
+        print("Mail send failed")
